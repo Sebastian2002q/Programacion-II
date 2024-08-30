add76b9ff7d4335d21ee84c2ae7f747f23132e62
Sebastian <sebastian.quinterof@uqvirtual.edu.co>
2024-08-30 08:51:31 -0500

    Estructura proyecto

--- co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/empresaTransporte/model/Empresa.java
+++ co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/empresaTransporte/model/Empresa.java
@@ -1,4 +1,9 @@
 package co.edu.uniquindio.empresaTransporte.model;
 
+import java.util.LinkedList;
+
 public class Empresa {
+    private LinkedList<Empleado> listEmpleados;
+    private LinkedList<Proyecto> listProyectos;
+    private LinkedList<Departamento> listDepartamentos;
 }
---
+++ co.edu.uniquindio.programacion2/src/main/java/co/edu/uniquindio/empresaTransporte/services/IContribuyente.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.empresaTransporte.services;
+
+public interface IContribuyente {
+}
